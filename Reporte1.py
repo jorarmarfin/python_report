fea55ef1251fff98d0c4be6ec4525e955d72833e
Luis Mayta <luis.mayta@gmail.com>
2022-12-09 22:55:15 -0500

    conexion mysql

--- Reporte1.py
+++ Reporte1.py
@@ -1,6 +1,15 @@
 from reportlab.pdfgen import canvas
 from reportlab.lib.utils import ImageReader
 from reportlab.lib.pagesizes import A4, letter,landscape
+from MySQLdb import _mysql
+
+#############DB################
+db=_mysql.connect("localhost","root","r2d23poLui$","personal")
+db.query("SELECT nombre FROM actividad where id <3 ")
+r=db.store_result()
+
+#################################
+
 # from import psycopg2
 #Instalar
 #pip install psycopg2
@@ -54,40 +63,14 @@ nombre.setFont(psfontname="Helvetica-Bold",size=10)
 nombre.textLine("NOMBRES")
 c.drawText(nombre)
 #
-sexo = c.beginText(x+220, h - 130)
-sexo.setFont(psfontname="Helvetica-Bold",size=10)
-sexo.textLine("SEXO")
-c.drawText(sexo)
-#
-especialidad = c.beginText(x+270, h - 130)
-especialidad.setFont(psfontname="Helvetica-Bold",size=10)
-especialidad.textLine("ESPECIALIDAD")
-c.drawText(especialidad)
-#
-departamento = c.beginText(x+370, h - 130)
-departamento.setFont(psfontname="Helvetica-Bold",size=10)
-departamento.textLine("DEPARTAMENTO/PROVINCIA/DISTRITO")
-c.drawText(departamento)
-#
-departamento = c.beginText(x+580, h - 130)
-departamento.setFont(psfontname="Helvetica-Bold",size=10)
-departamento.textLine("COLEGIO")
-c.drawText(departamento)
-#
-departamento = c.beginText(x+650, h - 130)
-departamento.setFont(psfontname="Helvetica-Bold",size=10)
-departamento.textLine("GESTION")
-c.drawText(departamento)
-#
-departamento = c.beginText(x+730, h - 130)
-departamento.setFont(psfontname="Helvetica-Bold",size=10)
-departamento.textLine("NOTA")
-c.drawText(departamento)
-#
 c.line(x+40, y - 90, w - 30, y - 90)
 incremento = 20
 #
-#
+print (type(r))
+# registro = r.fetchone()
+# while (registro != None):
+#     print registro['nombre']
+ 
 indice = c.beginText(x + 60, h - 130 - incremento)
 indice.setFillColorRGB(0,0,0)
 indice.setFont(psfontname="Helvetica",size=10)
@@ -98,37 +81,7 @@ nombre = c.beginText(x+80, h - 130  - incremento)
 nombre.setFont(psfontname="Helvetica",size=10)
 nombre.textLine("Luis Fernando Mayta Campos")
 c.drawText(nombre)
-#
-sexo = c.beginText(x+220, h - 130  - incremento)
-sexo.setFont(psfontname="Helvetica",size=10)
-sexo.textLine("Masculino")
-c.drawText(sexo)
-#
-especialidad = c.beginText(x+270, h - 130  - incremento)
-especialidad.setFont(psfontname="Helvetica",size=10)
-especialidad.textLine("Ing. Sistemas")
-c.drawText(especialidad)
-#
-departamento = c.beginText(x+370, h - 130  - incremento)
-departamento.setFont(psfontname="Helvetica",size=10)
-departamento.textLine("Lima/Lima/San Juan de Lurigancho")
-c.drawText(departamento)
-#
-departamento = c.beginText(x+580, h - 130  - incremento)
-departamento.setFont(psfontname="Helvetica",size=9)
-departamento.textLine("Santo Domingo de Guzman")
-c.drawText(departamento)
-#
-departamento = c.beginText(x+650, h - 130  - incremento)
-departamento.setFont(psfontname="Helvetica",size=10)
-departamento.textLine("Privada")
-c.drawText(departamento)
-#
-departamento = c.beginText(x+730, h - 130  - incremento)
-departamento.setFont(psfontname="Helvetica",size=10)
-departamento.textLine("18.23")
-c.drawText(departamento)
-#
+
 
 
 
